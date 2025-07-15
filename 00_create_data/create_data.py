ca3ff3543aff326a50dbfdbc5a28fd4d55545bcf
Matthias Motl <matthias@motl.de>
2025-07-15 12:21:40 +0200

    create data script finalized plus mvp.md

--- 00_create_data/create_data.py
+++ 00_create_data/create_data.py
@@ -9,19 +9,28 @@ Usage:
     python create_data.py <n> <C> <cluster_std> <RSEED> [<filename>]
 where:
     n: Number of samples
-    C: Number of clusters
     cluster_std: Standard deviation of clusters
     RSEED: Random seed for reproducibility
     filename: Output filename
+    not used: C: Number of clusters, centroids are initialized in the code
     '''
 
-n = int(sys.argv[1])  # Number of samples
-C = int(sys.argv[2])  # Number of clusters
-cluster_std = float(sys.argv[3])  # Standard deviation of clusters
-RSEED = int(sys.argv[4])  # Random seed for reproducibility
-filename = sys.argv[5] if len(sys.argv) > 0 else 'data.csv'  # Output filename
+n = int(sys.argv[1])  # Number of samples (500 in the original code)
+cluster_std = float(sys.argv[2])  # Standard deviation of clusters (2 in the original code)
+RSEED = int(sys.argv[3])  # Random seed for reproducibility 
+filename = sys.argv[4] if len(sys.argv) > 0 else 'data.csv'  # Output filename
+# C = int(sys.argv[5])  # Number of clusters (3 in the original code)
 
-def create_data(n, C, cluster_std, RSEED):
+# initialization of centroids
+# These centroids are used to generate the synthetic data.
+centroids = [[-2.50919762,  9.01428613,  4.63987884,  1.97316968, -6.87962719,
+        -6.88010959, -8.83832776,  7.32352292,  2.02230023,  4.16145156],
+       [-9.58831011,  9.39819704,  6.64885282, -5.75321779, -6.36350066,
+        -6.3319098 , -3.91515514,  0.49512863, -1.36109963, -4.1754172 ],
+       [ 2.23705789, -7.21012279, -4.15710703, -2.67276313, -0.87860032,
+         5.70351923, -6.00652436,  0.28468877,  1.84829138, -9.07099175]]
+
+def create_data(n, cluster_std, RSEED, C):
     '''Generate synthetic data using make_blobs and return a DataFrame.
     Args:
         n (int): Number of samples.
@@ -47,13 +56,13 @@ def save_data(df, filename=filename):
         filename (str): Name of the output CSV file.
     Returns:
         str: The filename where the data is saved.'''
-    df.to_csv(filename, index=False)
+    df.to_csv(f'../data/{filename}', index=False)
     print(f"Data saved to {filename}")
     return filename
 
 
 if __name__ == "__main__":
     '''Main function to create and save the data.'''
-    df = create_data(n, C, cluster_std, RSEED)
+    df = create_data(n, cluster_std, RSEED, C=centroids)
     filename = save_data(df)
     print(f"Dataframe shape: {df.shape}")
---
+++ mvp.md
@@ -0,0 +1,22 @@
+
+- data: creation in jupyter notebooks / to_scripts
+    reference_data (model trained on)
+    new_data (to run labeling and to detect drifts)
+
+- model: experiment tracking
+    - creation in jupyter notebooks / to_scripts
+    - experiment tracking in mlflow, locally
+    - model registry locally, sqlite
+
+- no pipe
+- no deploy
+
+- monitoring: evidently: data drift and silouhette score, locally in notebooks
+
+- no reproducibility
+
+- best practice:
+    - pre-commit hooks: large_files / private_key
+    - TBD black
+
+
